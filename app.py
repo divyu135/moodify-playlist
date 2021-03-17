7320b1b8e95ab4c7ef383a06dcb433db4e592b3f
Divyajeet Chauhan <divyajeetchauhan@gmail.com>
2021-03-17 15:00:12 -0400

    modify app.py and .gitignore

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+from api.lyrics_scrapper import GetLyrics
+from decouple import config
+
+genius_api = config('genius_api')
+ibm_api = config('ibm_api')
 
 app = Flask(__name__)
 CORS(app)
@@ -7,3 +12,10 @@ CORS(app)
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Hello, welcome to my world"}
+
+@app.route("/lyrics")
+def display_lyrics():
+    songs = GetLyrics(genius_api)
+    song_lyrics = songs.get_lyrics(["old town road"],["Lil Nas X"]) 
+    song_json =  { "text":song_lyrics[0].replace("\n"," ")}
+    return(song_json)
