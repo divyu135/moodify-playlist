3ea78dae8166c8e81d39335d6403e2a9318a6b70
Vyoma Kanani <vyomapatel01@gmail.com>
2021-03-18 21:41:37 -0400

    Update about and contact page

--- react-moodify/src/Components/About.js
+++ react-moodify/src/Components/About.js
@@ -17,19 +17,47 @@ class About extends Component {
     }
 
     return (
+       <div>
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+         <h2>About US</h2>
          </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
+      </div>
+      <br />
+      
 
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
+      <div className="row">
+      <hr />
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+            <p>{bio}</p>      
+         </div>
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+            <p>{bio}</p>      
+         </div>
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+            <p>{bio}</p>      
+         </div>
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
+            <p>{bio}</p>      
+         </div>
+      </div>
+   </section>
+
+   <section id="contact">
+      <div className="row">
+         <div className="three columns">
+         <h2>Contact Details</h2>
+         </div>
+      </div>
+      <br />
+      <div className="row">
+      <hr />
+      <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
 						         {city} {state}, {zip}
@@ -37,17 +65,9 @@ class About extends Component {
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
       </div>
-
    </section>
+   </div>
     );
   }
 }
