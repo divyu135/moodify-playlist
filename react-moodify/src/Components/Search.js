eee6b48129ef182a6ba0c129c9e95f0df4094d51
Hussain-MD <71483457+Hussain-MD@users.noreply.github.com>
2021-03-18 22:00:42 -0400

    search page

--- react-moodify/src/Components/Search.js
+++ react-moodify/src/Components/Search.js
@@ -1,84 +1,84 @@
-import React from 'react';
-import ParticlesBg  from "particles-bg";
+import React, {useState} from 'react';
+import ParticlesBg from "particles-bg";
+import ListBox from './ListBox';
 import '../App.css';
 
-class Search extends React.Component {
-  state = {
-    search_text: ''
-  };
+const Search = () => {
+  const [search, setSearch] = useState('');
+  const [listOfResults, setResults] = useState([]);
 
-  handleSearch = e => {
-    e.preventDefault();
-    fetch('http://localhost:5000/lyrics', {
+
+  const handleSearch = e => {
+    // e.preventDefault();
+    fetch('http://localhost:5000/search', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        username: this.state.search_text
+        search_text: search
       })
     })
       .then(response => response.json())
       .then(json => {
-        console.log(json.text);
+        setResults(json.response.hits)
+        // console.log(listOfResults)
       })
     //   .catch(error => {
     //     this.props.onLoginError();
     //   });
   };
 
-  handleSearchChange = e => {
-    this.setState({
-      search_text: e.target.value
-    });
+  const handleSearchChange = e => {
+    setSearch(e.target.value)
   };
 
-
-  render() {
-    return (
-        
-        <header id="home">
+  return (
+    <React.Fragment>
+      <header id="home">
         <ParticlesBg type="circle" bg={true} />
         <nav id="nav-wrap">
-           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-  
-           <ul id="nav" className="nav">
-              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-              <li><a className="smoothscroll" href="#about">About</a></li>
-              <li><a className="smoothscroll" href="#about">Contact</a></li>
-           </ul>
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+          <ul id="nav" className="nav">
+            <li><a  href="/home">Home</a></li>
+            <li className="current"><a className="smoothscroll" href="#">Search</a></li>
+            {/* <li><a className="smoothscroll" href="#about">Contact</a></li> */}
+          </ul>
         </nav>
-        
-        
+
+
         <div className="row banner">
-      
-         <div className="banner-text">
+
+          <div className="banner-text">
             {/* <h1 className="responsive-headline"> Moodify: Lets Moodify Your Playlist</h1> */}
-            <h3>Search a song and we will find the mood based on that song for you. And create playlist
+            <h3>Search a song and we will find the mood based on that song for you. Then we will create playlist
             based on the found mood.</h3>
             <hr />
-            <div 
-                style = {{ display: "flex", justifyContent: "center"}}
-            >
-            <input
-            type="text"
-            name="search_text"
-            className="banner-text"
-            style= {{width:"50%"}}
-            value={this.state.search_text}
-            onChange={this.handleSearchChange}
-            />
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <input
+                type="text"
+                name="search_text"
+                className="banner-text"
+                style={{ width: "50%" }}
+                value={search}
+                onChange={handleSearchChange}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    handleSearch()
+                  }
+                }}
+              />
             </div>
-            <button className="button btn github-btn" onClick={this.handleSearch}>Search</button>
-         </div>
-      </div>
-        
-        </header>  
-      
-      
-    );
-  }
+            <button className="button btn github-btn" onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+      </header>
+      <ListBox hits={listOfResults} />
+    </React.Fragment>
+  );
 }
 
 export default Search;
